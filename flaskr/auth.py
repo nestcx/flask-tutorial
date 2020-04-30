abfcf3ee7dbbe9a8622d7077cc563a6e43987d54
nestcx <marcusvalentino2013@gmail.com>
2020-04-30 23:38:17 +1000

    fix error in db execute statement

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -47,7 +47,7 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute('SELECT * FROM user WHERE username = ?', (username,))fetchone()
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
 
         if user is None:
             error = 'Incorrect username.'
