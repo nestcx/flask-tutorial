b580b25ec574ff31a25158f2cd7715e13cc260ee
nestcx <marcusvalentino2013@gmail.com>
2020-04-30 21:51:17 +1000

    add auth.py and complete auth code

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    print("name: " + __name__)
     app.config.from_mapping(
             SECRET_KEY='dev',
             DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -29,5 +30,8 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
